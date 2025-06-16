12d3910aa68aae166473277b95c1138f060ab324
moldenburger <maritoldenburger@hotmail.com>
2025-06-16 17:06:24 +0200

    Methodes toegevoegd aan GrassPokemon

--- src/GrassPokemon.java
+++ src/GrassPokemon.java
@@ -1,2 +1,85 @@
 public class GrassPokemon extends Pokemon {
+
+    private final String type = "electric";
+
+    public GrassPokemon(String name, int level, int hp, String food, String sound) {
+        super(name, level, hp, food, sound, type);
+    }
+
+    public void leafStorm(Pokemon name, Pokemon enemy) {
+        System.out.println(name.getName() + " attacks " + enemy.getName() + " with Leaf Storm!");
+        switch (enemy.getType()) {
+            case "electric":
+                System.out.println(enemy.getName() + " loses 50 hp");
+                enemy.setHp(enemy.getHp() - 50);
+            case "fire":
+                System.out.println(enemy.getName() + " loses 25 hp");
+                enemy.setHp(enemy.getHp() - 25);
+            case "grass":
+                System.out.println(enemy.getName() + " loses 5 hp");
+                enemy.setHp(enemy.getHp() - 5);
+            case "water":
+                System.out.println(enemy.getName() + " loses 10 hp");
+                enemy.setHp(enemy.getHp() - 10);
+        }
+        System.out.println(enemy.getName() + " has " + enemy.getHp() + " hp left");
+    }
+
+    public void solarBeam(Pokemon name, Pokemon enemy) {
+        System.out.println(name.getName() + " attacks " + enemy.getName() + " with Solar Beam!");
+        switch (enemy.getType()) {
+            case "electric":
+                System.out.println(enemy.getName() + " loses 100 hp");
+                enemy.setHp(enemy.getHp() - 100);
+            case "fire":
+                System.out.println(enemy.getName() + " loses 50 hp");
+                enemy.setHp(enemy.getHp() - 50);
+            case "grass":
+                System.out.println(enemy.getName() + " loses 10 hp");
+                enemy.setHp(enemy.getHp() - 10);
+            case "water":
+                System.out.println(enemy.getName() + " loses 25 hp");
+                enemy.setHp(enemy.getHp() - 25);
+        }
+        System.out.println(enemy.getName() + " has " + enemy.getHp() + " hp left");
+    }
+
+    public void leechSeed(Pokemon name, Pokemon enemy) {
+        System.out.println(name.getName() + " attacks " + enemy.getName() + " with Leech Seed!");
+        switch (enemy.getType()) {
+            case "electric":
+                System.out.println(enemy.getName() + " loses 50 hp");
+                enemy.setHp(enemy.getHp() - 50);
+            case "fire":
+                System.out.println(enemy.getName() + " loses 25 hp");
+                enemy.setHp(enemy.getHp() - 25);
+            case "grass":
+                System.out.println(enemy.getName() + " loses 5 hp");
+                enemy.setHp(enemy.getHp() - 5);
+            case "water":
+                System.out.println(enemy.getName() + " loses 10 hp");
+                enemy.setHp(enemy.getHp() - 10);
+        }
+        System.out.println(enemy.getName() + " has " + enemy.getHp() + " hp left");
+    }
+
+    public void leaveBlade(Pokemon name, Pokemon enemy) {
+        System.out.println(name.getName() + " attacks " + enemy.getName() + " with Leave Blade!");
+        switch (enemy.getType()) {
+            case "electric":
+                System.out.println(enemy.getName() + " loses 100 hp");
+                enemy.setHp(enemy.getHp() - 100);
+            case "fire":
+                System.out.println(enemy.getName() + " loses 50 hp");
+                enemy.setHp(enemy.getHp() - 50);
+            case "grass":
+                System.out.println(enemy.getName() + " loses 10 hp");
+                enemy.setHp(enemy.getHp() - 10);
+            case "water":
+                System.out.println(enemy.getName() + " loses 25 hp");
+                enemy.setHp(enemy.getHp() - 25);
+        }
+        System.out.println(enemy.getName() + " has " + enemy.getHp() + " hp left");
+    }
+
 }
